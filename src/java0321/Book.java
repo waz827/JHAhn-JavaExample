67aa1b5927cf6f515a59a51a1da702366765da04
JC <waz827@naver.com>
2023-03-21 16:46:15 +0900

    Java Example

---
+++ src/java0321/Book.java
@@ -0,0 +1,23 @@
+package java0321;
+
+public class Book {
+    String title;
+    String author;
+    void show(){
+        System.out.println(title +""+author);
+    }
+    public Book(){
+        this("","홍길동");
+    }
+    public Book(String title){
+        this(title,"  작자미상");
+    }
+    public Book(String title, String author){
+        this.title= title;
+        this.author= author;
+    }
+    public static void main(String[] args) {
+        Book emptyBook= new Book("자바를배우자");
+        emptyBook.show();
+    }
+}
