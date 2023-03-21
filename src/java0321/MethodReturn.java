8345b05702ffb3e320f700a0b964f50b43e40a06
JC <waz827@naver.com>
2023-03-21 15:56:48 +0900

    Java Example

---
+++ src/java0321/MethodReturn.java
@@ -0,0 +1,24 @@
+package java0321;
+
+import java.sql.SQLOutput;
+
+public class MethodReturn {
+    public static int[] makeArray(){
+        int temp[]=new int[20];
+        for(int i=0; i<temp.length; i++){
+            temp[i]=i;
+        }
+        return temp;
+    }
+    public static void main(String[] args) {
+        int intArray[]=makeArray();
+        int sum=0;
+        for(int i:intArray){
+            sum +=i;
+            System.out.println(i);
+        }
+        System.out.println();
+        System.out.println(sum);
+
+    }
+}
